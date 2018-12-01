01878cc166f0bc413b64719d62f4c0bbe0ce59ad
Wing-ka-king <singhakash1298@gmail.com>
2018-12-01 05:41:42 +0100

    build

---
+++ Publishers and Subscribers/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/akash/Documents/Work/Sweden/FormulaStudent/Training/kthfsdv/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/akash/Documents/Work/Sweden/FormulaStudent/Training/kthfsdv/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/akash/Documents/Work/Sweden/FormulaStudent/Training/kthfsdv/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/akash/Documents/Work/Sweden/FormulaStudent/Training/kthfsdv/src:$ROS_PACKAGE_PATH"
